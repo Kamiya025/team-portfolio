6f475fc65828b1953e4542e9251ce86b93cec76a
Kamiya025 <hawk01525@gmail.com>
2025-09-22 14:16:07 +0700

    feat: thêm Google Tag Manager vào layout để theo dõi và phân tích lưu lượng truy cập, cải thiện khả năng quản lý mã theo dõi.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/providers/theme-provider"
 import { hasLocale, NextIntlClientProvider } from "next-intl"
 import { Inter } from "next/font/google"
 import { notFound } from "next/navigation"
+import Script from "next/script"
 const inter = Inter({
   subsets: ["latin", "latin-ext"],
   variable: "--font-inter",
@@ -26,11 +27,32 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} className="dark">
       <body className={`${inter.className} dark:bg-gray-900 h-screen`}>
-        <NextIntlClientProvider>
+        {/* Google Tag Manager */}
+        {process.env.NEXT_PUBLIC_GTM_ID && (
+          <>
+            <Script id="gtm-base" strategy="afterInteractive">
+              {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');`}
+            </Script>{" "}
+            <noscript>
+              <iframe
+                src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+          </>
+        )}
+        {/* End Google Tag Manager */}
+        {/* <NextIntlClientProvider>
           <ThemeProvider defaultTheme="dark" storageKey="team-portfolio-theme">
             {children}
           </ThemeProvider>
-        </NextIntlClientProvider>
+        </NextIntlClientProvider> */}
       </body>
     </html>
   )
