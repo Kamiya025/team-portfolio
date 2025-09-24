4ec23bb7157fe604ef172dd5207f6e07197ea5c7
Kamiya025 <hawk01525@gmail.com>
2025-09-24 09:49:20 +0700

    thay đổi tên nhóm trong WebContent từ 'TeamPortfolio' thành 'Vnest'.

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,31 +1,31 @@
-import type { SocialLink, FooterContent } from "@/models"
+import type { SocialLink, FooterContent } from '@/models'
 
 export const socialLinks: SocialLink[] = [
   {
-    name: "Facebook",
-    href: "#",
-    icon: "facebook",
+    name: 'Facebook',
+    href: '#',
+    icon: 'facebook',
   },
   {
-    name: "Twitter",
-    href: "#",
-    icon: "twitter",
+    name: 'Twitter',
+    href: '#',
+    icon: 'twitter',
   },
   {
-    name: "LinkedIn",
-    href: "#",
-    icon: "linkedin",
+    name: 'LinkedIn',
+    href: '#',
+    icon: 'linkedin',
   },
   {
-    name: "GitHub",
-    href: "#",
-    icon: "github",
+    name: 'GitHub',
+    href: '#',
+    icon: 'github',
   },
 ]
 
 export const WebContent: FooterContent = {
-  teamName: "TeamPortfolio",
+  teamName: 'Vnest',
   description:
-    "Chúng tôi là đội ngũ chuyên gia công nghệ với nhiều năm kinh nghiệm, cam kết mang đến những giải pháp công nghệ tối ưu cho doanh nghiệp.",
-  copyright: "Tất cả quyền được bảo lưu.",
+    'Chúng tôi là đội ngũ chuyên gia công nghệ với nhiều năm kinh nghiệm, cam kết mang đến những giải pháp công nghệ tối ưu cho doanh nghiệp.',
+  copyright: 'Tất cả quyền được bảo lưu.',
 }
